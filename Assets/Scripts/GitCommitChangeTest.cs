2ba84c8a68a111bc6a387c1649f7558460227d97
Xavian Escamilla <xzillagaming@gmail.com>
2024-01-24 18:05:07 -0600

    Testing File Update in Git

--- Assets/Scripts/GitCommitChangeTest.cs
+++ Assets/Scripts/GitCommitChangeTest.cs
@@ -7,7 +7,7 @@ public class GitCommitChangeTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("This is a test script to check commit changes");
     }
 
     // Update is called once per frame
